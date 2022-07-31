ab708bf5f50662d559db8639ebc3f03fe396f711
Hayet Kumasseh <naaakleh@gmail.com>
2022-07-31 12:47:05 -0400

    commit tree 5f04d20bbb7e3988cea72b0c599561b1adca8875

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,16 @@
 class ApplicationController < ActionController::API
   include ActionController::Cookies
+  # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+  # rescue_from ActiveRecord::RecordInvalid, with: :render_unprocessable_entity_response
+  
+
+  # # private
+  # def render_not_found_response(invalid)
+  #   render json: { error: "#{invalid.model} not found" }, status: :not_found
+  # end
+  
+  # def render_unprocessable_entity_response(invalid)
+  #   render json: { error: invalid.record.errors.full_messages }, status: :unprocessable_entity
+  # end
 
 end
